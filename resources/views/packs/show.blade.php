34e6a24647183365dd673195bd505faa652910c1
louis <Louis.Kelly1@hotmail.com>
2020-12-06 00:07:35 +0000

    Pack view first draft complete

--- resources/views/packs/show.blade.php
+++ resources/views/packs/show.blade.php
@@ -2,8 +2,17 @@
 
 @section('content')
 
-<div class="container d-flex justify-content-center">
-  <div class="h1">{{ $id }}</div>
+<div class="container">
+  <div class="h1 text-center">Label for {{ $id }}</div>
+  <div class="card">
+    <h5 class="card-header">Malign</h5>
+    <div class="card-body">
+      <h5 class="card-subtitle font-italic">adjective</h5>
+      <p class="card-text definition">evil in nature or effect</p>
+      <h5 class="card-subtitle font-italic">verb</h5>
+      <p class="card-text definition">speak about (someone) in a spitefully critical manner.</p>
+    </div>
+  </div>
 </div>
 
 @endsection
